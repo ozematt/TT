ec751d9d88ff51d10e447064d2a11a77a47e2279
Mateusz Obirek <neutralnie7@gmail.com>
2024-09-11 09:51:36 +0200

    added test e2e

--- app/e2e/AddTask.spec.ts
+++ app/e2e/AddTask.spec.ts
@@ -1,11 +1,12 @@
 import { test, expect } from "@playwright/test";
 
+const URL = "http://localhost:81/";
+
 test.describe("Add new task flow", () => {
-  test("Add new task test", async ({ page }) => {
-    await page.goto("http://localhost:81/");
+  test("new task flow test", async ({ page }) => {
+    await page.goto(URL);
 
     await page.getByRole("button", { name: "ADD TASK" }).click();
-
     await page
       .getByRole("textbox", { name: "Code" })
       .fill("next-task-test-e2e");
@@ -13,11 +14,14 @@ test.describe("Add new task flow", () => {
     await page
       .getByRole("textbox", { name: "Description" })
       .fill("task-description-e2e");
-
     await page.getByRole("button", { name: "ADD" }).click();
 
-    await expect(
-      page.getByText("next-task-test-e2e ::  task-title-e2e ( 0h 0min )")
-    ).toBeVisible();
+    const task = page.getByText(
+      "next-task-test-e2e ::  task-title-e2e ( 0h 0min )"
+    );
+    await expect(task).toBeVisible();
+    await page.getByTitle("Start").nth(1).click();
+    await page.getByTitle("Close task").nth(1).click();
+    await page.getByTitle("Delete task").nth(2).click();
   });
 });
